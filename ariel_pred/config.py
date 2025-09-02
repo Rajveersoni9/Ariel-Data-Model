607aaa1aa94e0fee3b97eaa9c39e7f1b9b35d284
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2025-09-02 22:29:52 +0300

    Refactor config to a class

--- ariel_pred/config.py
+++ ariel_pred/config.py
@@ -6,20 +6,22 @@ from loguru import logger
 # Load environment variables from .env file if it exists
 load_dotenv()
 
-# Paths
-PROJ_ROOT = Path(__file__).resolve().parents[1]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
-DATA_DIR = PROJ_ROOT / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
-INTERIM_DATA_DIR = DATA_DIR / "interim"
-PROCESSED_DATA_DIR = DATA_DIR / "processed"
-EXTERNAL_DATA_DIR = DATA_DIR / "external"
+class Config:
+    """Configuration for the project paths and settings."""
 
-MODELS_DIR = PROJ_ROOT / "models"
+    PROJ_ROOT = Path(__file__).resolve().parents[1]
+    DATA_DIR = PROJ_ROOT / "data"
+    RAW_DATA_DIR = DATA_DIR / "raw"
+    INTERIM_DATA_DIR = DATA_DIR / "interim"
+    PROCESSED_DATA_DIR = DATA_DIR / "processed"
+    EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
-REPORTS_DIR = PROJ_ROOT / "reports"
-FIGURES_DIR = REPORTS_DIR / "figures"
+    MODELS_DIR = PROJ_ROOT / "models"
+
+    REPORTS_DIR = PROJ_ROOT / "reports"
+    FIGURES_DIR = REPORTS_DIR / "figures"
+
 
 # If tqdm is installed, configure loguru with tqdm.write
 # https://github.com/Delgan/loguru/issues/135
