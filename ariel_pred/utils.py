c59be5a13d3e073206d179dec6c9910c55542eb5
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2025-09-14 17:10:30 +0300

    check smoothing effects on predictions

--- ariel_pred/utils.py
+++ ariel_pred/utils.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
 import torch
 
 
@@ -9,3 +13,36 @@ def get_device_from_str(device_str: str) -> torch.device:
     if device_str == "cuda" and not torch.cuda.is_available():
         raise ValueError("CUDA device is not available on this machine.")
     return torch.device(device_str)
+
+
+def test_submission_maker(
+    input_data_path: Path, predictions: np.ndarray, sigma: float, multiplier: float = 1.0
+) -> pd.DataFrame:
+    result = np.concatenate(
+        [predictions.clip(min=0) * multiplier, np.full(predictions.shape, sigma)], axis=1
+    )
+    assert result.shape == (predictions.shape[0], 283 * 2), (
+        f"Result shape {result.shape} is not correct"
+    )
+    submission_df = pd.read_csv(input_data_path / "sample_submission.csv")
+    submission_df.iloc[:, 1:] = result
+    return submission_df
+
+
+def train_submission_maker(
+    input_data_path: Path, predictions: np.ndarray, sigma: float, multiplier: float = 1.0
+) -> pd.DataFrame:
+    result = np.concatenate(
+        [predictions.clip(min=0.0) * multiplier, np.full(predictions.shape, sigma)], axis=1
+    )
+    assert result.shape == (predictions.shape[0], 283 * 2), (
+        f"Result shape {result.shape} is not correct"
+    )
+    sample_submission_df = pd.read_csv(input_data_path / "sample_submission.csv")
+    planet_ids = pd.read_csv(input_data_path / "train.csv")["planet_id"].values
+
+    submission_df = pd.DataFrame(
+        np.concatenate([planet_ids[:, np.newaxis], result], axis=1), # type: ignore
+        columns=sample_submission_df.columns,
+    )
+    return submission_df
