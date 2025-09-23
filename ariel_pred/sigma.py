90dbce023f802d3affdbe7af8ffd66bcb7b90532
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2025-09-23 17:41:22 +0300

    Experimented with new sigma calculation technique for FGS

--- ariel_pred/sigma.py
+++ ariel_pred/sigma.py
@@ -108,3 +108,68 @@ class OGSignalVarBasedSigmaCalculator:
         )
 
         return sigma
+
+
+class WhiteCurveVarBasedSigmaCalculator:
+    def __init__(
+        self,
+        mean_fgs_sigma: float = 8.5e-4,
+        mean_airs_sigma: float = 5.0e-4,
+        fgs_min_sigma: float = 1e-6,
+        airs_min_sigma: float = 1e-6,
+        smoother: SGSmoothing = SGSmoothing(window_size=150, poly_order=2),
+        transit_finder: FunctionFittingBasedPhaseDetector = FunctionFittingBasedPhaseDetector(),
+        eps: float = 1e-12,
+    ):
+        self.mean_fgs_sigma = mean_fgs_sigma
+        self.mean_airs_sigma = mean_airs_sigma
+        self.fgs_min_sigma = fgs_min_sigma
+        self.airs_min_sigma = airs_min_sigma
+        self.smoother = smoother
+        self.transit_finder = transit_finder
+        self.eps = eps
+
+    def _get_sigma(self, data: np.ndarray, transit_locations: np.ndarray):
+        sigma = np.zeros(data.shape[0])
+
+        for i in tqdm(range(data.shape[0]), desc="Calculating AIRS sigma"):
+            signal = data[i]
+            t1, t2, t3, t4 = transit_locations[i]
+
+            unsmoothed_white_curve = signal[:, 1:].mean(axis=1)
+
+            oot = np.concatenate(
+                [unsmoothed_white_curve[:t1], unsmoothed_white_curve[t4:]], axis=0
+            )
+            inn = unsmoothed_white_curve[t2:t3]
+
+            var_oot = np.var(oot)
+            var_inn = np.var(inn)
+            oot_mean = np.mean(oot)
+
+            sigma[i] = np.sqrt((var_inn / len(inn) + var_oot / len(oot)).mean()) / max(
+                oot_mean, self.eps
+            )
+
+        sigma = np.repeat(sigma[:, np.newaxis], 283, axis=1)
+        sigma[:, 0] = sigma[:, 0] / np.mean(sigma[:, 0]) * self.mean_fgs_sigma
+        sigma[:, 0] = sigma[:, 0].clip(min=self.fgs_min_sigma)
+        sigma[:, 1:] = sigma[:, 1:] / np.mean(sigma[:, 1:]) * self.mean_airs_sigma
+        sigma[:, 1:] = sigma[:, 1:].clip(min=self.airs_min_sigma)
+        return sigma
+
+    def get_sigma(self, data: np.ndarray):
+        assert data.ndim == 3 and data.shape[2] == 283, (
+            "Data should be of shape (num_planets, num_time_steps, 283)"
+        )
+
+        smoothed_white_curves = np.zeros((data.shape[0], data.shape[1]))
+        for i in range(data.shape[0]):
+            smoothed_white_curves[i] = self.smoother.smooth(data[i, :, 1:].mean(axis=1))
+        transit_locations = self.transit_finder.phase_detect_multiple_planets(
+            smoothed_white_curves
+        )
+
+        sigma = self._get_sigma(data.copy(), transit_locations)
+
+        return sigma
